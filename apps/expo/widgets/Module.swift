2fadaf35d67819da2d50867bada9f64d152a1609
Matt Calayo <matthewcalayo@gmail.com>
2026-01-26 22:35:36 -0500

    proper widget extension class

--- apps/expo/widgets/Module.swift
+++ apps/expo/widgets/Module.swift
@@ -1,9 +1,25 @@
+import ExpoModulesCore
+import ActivityKit
 import WidgetKit
 
-@objc(TodoWidgetModule)
-class TodoWidgetModule: NSObject {
-    @objc
-    static func requiresMainQueueSetup() -> Bool {
-        return false
+public class ReactNativeWidgetExtensionModule: Module {
+    public func definition() -> ModuleDefinition {
+        Name("ReactNativeWidgetExtension")
+
+        Function("areActivitiesEnabled") { () -> Bool in
+            if #available(iOS 16.2, *) {
+                return ActivityAuthorizationInfo().areActivitiesEnabled
+            } else {
+                return false
+            }
+        }
+
+        Function("reloadAllTimelines") { () -> Void in
+            WidgetCenter.shared.reloadAllTimelines()
+        }
+
+        Function("reloadTimelines") { (kind: String) -> Void in
+            WidgetCenter.shared.reloadTimelines(ofKind: kind)
+        }
     }
 }
