ed09de1f5e3f685a6a885b8b96b1383b11b65372
Matt Calayo <matthewcalayo@gmail.com>
2026-02-25 14:12:21 -0500

    mobile popover

--- packages/ui/src/sidebar.tsx
+++ packages/ui/src/sidebar.tsx
@@ -189,7 +189,7 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="bg-sidebar text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
+          className="bg-popover text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
           style={
             {
               "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
