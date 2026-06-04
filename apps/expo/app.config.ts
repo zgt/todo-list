63bca00a683bfb5d2ae672fe72eaaa515ba4a4ed
Matt Calayo <matthewcalayo@gmail.com>
2026-06-04 14:54:45 -0400

    slug back

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -4,7 +4,7 @@ import type { ConfigContext, ExpoConfig } from "expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Tokilist",
-  slug: "todolist",
+  slug: "tokilist",
   scheme: "tokilist",
   version: "1.0.0",
   orientation: "portrait",
